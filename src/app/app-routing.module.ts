989da3014cc3b98651fae35c794b3dc681c4cc20
Jeni Dimitrova <jeni@Jenis-MacBook.local>
2019-01-21 10:32:48 +0100

    front page bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TestSnakeAppComponent } from './Components/test-snake-app/test-snake-app.component';
+import {Router} from '@angular/router';
 
-
+// this.router= Router;
+// this.router.navigateByUrl('/snakegame');
 
 
 const routes: Routes = [
-  { path: '', 
-    redirectTo: 'home', 
-    pathMatch: 'full' 
-  },
 
   { path: 'snakegame',
     component: TestSnakeAppComponent
-  }
+  },
+  //  { path: '',
+  //    redirectTo: '/',
+  //    pathMatch: 'full'
+  //  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
 })
 export class AppRoutingModule { 
 
